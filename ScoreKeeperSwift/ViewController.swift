ebffefaec31367582e614054aa734e5516d38ca4
Marcus Mazur <marcus.mazur@tretton37.com>
2015-01-31 15:17:35 +0100

    Initial commit

---
+++ ScoreKeeperSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ScoreKeeperSwift
+//
+//  Created by Marcus Mazur on 2015-01-31.
+//  Copyright (c) 2015 Marcus Mazur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var dataService: ScoreKeeperDataService = ScoreKeeperDataService();
+        
+        dataService.getRequest();
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
